80d72a8d035286a72bd9ef7293a49759f1806747
eliasall3x <elliasallex@gmail.com>
2020-12-01 22:28:58 -0300

    feat: win32 executablePath added

--- src/crawler.js
+++ src/crawler.js
@@ -1,10 +1,13 @@
 const puppeteer = require('puppeteer-core');
 const os = require('os');
+const path = require('path');
+
+const dirname = path.resolve(__dirname, '..', 'node_modules', 'puppeteer', '.local-chromium', 'win64-818858', 'chrome-win', 'chrome.exe');
 
 const executablePaths = {
   'linux': "usr/bin/google-chrome",
   'darwin': "Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
-  'win32': "../node_modules/puppeteer/.local-chromium/win64-818858/chrome-win"
+  'win32': dirname
 }
 
 const platform = os.platform();
@@ -13,7 +16,8 @@ const platform = os.platform();
   const browser = await puppeteer.launch({
     headless: true,
     executablePath: executablePaths[platform]
-  });
+  }).catch(err => console.warn(err.message));
+
   const page = await browser.newPage();
   await page.goto('https://www.youtube.com/watch?v=faekjlZuTFA&list=PL85ITvJ7FLohhULgUFkYBf2xcXCG6yfVV');
   await page.screenshot({
